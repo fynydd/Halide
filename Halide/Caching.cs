55176c979ed519edad25317be9fe0ea961c6b28d
Michael Argentini <michael@argentini.us>
2018-07-25 20:08:59 -0400

    Caching additions
    Added CachePermanent, CacheDelete(), typed Cache retrieval, context support

--- Halide/Caching.cs
+++ Halide/Caching.cs
@@ -7,22 +7,20 @@ namespace Fynydd.Halide
     /// <summary>
     /// The Caching class contains methods and properties for
     /// manipulating the page output cache, as well as other
-    /// caching and history functionality. Cache time is determined by the "OutputCacheTime" setting
+    /// caching and history functionality. Cache time is determined by the "OutputCache.Expiration.Seconds" setting
     /// in the Halide configuration section of the web.config file. The time is in seconds.
     /// <example>
     /// <code>
     /// <![CDATA[
     /// <configSections>
-    ///		<section name="Fynydd.Halide" type="System.Configuration.NameValueFileSectionHandler"/>
+    ///     <section name="Fynydd.Halide" type="System.Configuration.NameValueFileSectionHandler"/>
     /// </configSections>
     /// <Fynydd.Halide>
-    ///     <add key="OutputCacheTime" value="300"/>
-    ///		<add key="EncryptionBaseKey" value="10,64,9,2,13,99,67,118,241,133,116,30,217,183,146,18,56,74,90,36,26,129,81,219"/>
-    ///		<add key="EncryptionInitVector" value="162,199,183,54,126,11,114,221,16,187,122,221,151,9,176,75,119,17"/>
+    ///     <add key="OutputCache.Expiration.Seconds" value="300"/>
     /// </Fynydd.Halide>
-    ///	]]>
-    ///	</code>
-    ///	</example>
+    /// ]]>
+    /// </code>
+    /// </example>
     /// </summary>
     public static class Caching
     {
@@ -35,7 +33,7 @@ namespace Fynydd.Halide
         {
             get
             {
-                return Config.GetKeyValue<int>("OutputCacheTime", 300, "Fynydd.Halide");
+                return Config.GetKeyValue<int>("OutputCache.Expiration.Seconds", 300, "Fynydd.Halide");
             }
         }
 
@@ -49,9 +47,31 @@ namespace Fynydd.Halide
         /// <param name="key">Unique name of the cached item</param>
         /// <param name="value">Object to store in the cache</param>
         /// <param name="expirationSeconds">Expiration time, in seconds, from the current date and time.</param>
-        public static void Cache(string key, object value, int expirationSeconds)
+        /// <param name="context">HttpContext; defaults to Current</param>
+        public static void Cache(string key, object value, int expirationSeconds, HttpContext context = null)
         {
-            HttpContext.Current.Cache.Add(key, value, null, DateTime.Now.AddSeconds(Convert.ToDouble(expirationSeconds)), System.Web.Caching.Cache.NoSlidingExpiration, System.Web.Caching.CacheItemPriority.Normal, null);
+            if (context == null)
+            {
+                context = HttpContext.Current;
+            }
+
+            context.Cache.Add(key, value, null, DateTime.Now.AddSeconds(Convert.ToDouble(expirationSeconds)), System.Web.Caching.Cache.NoSlidingExpiration, System.Web.Caching.CacheItemPriority.Normal, null);
+        }
+
+        /// <summary>
+        /// Output Caching wrapper method. Caches an object for the life of the application.
+        /// </summary>
+        /// <param name="key">Unique name of the cached item</param>
+        /// <param name="value">Object to store in the cache</param>
+        /// <param name="context">HttpContext; defaults to Current</param>
+        public static void CachePermanent(string key, object value, HttpContext context = null)
+        {
+            if (context == null)
+            {
+                context = HttpContext.Current;
+            }
+
+            context.Cache.Add(key, value, null, System.Web.Caching.Cache.NoAbsoluteExpiration, System.Web.Caching.Cache.NoSlidingExpiration, System.Web.Caching.CacheItemPriority.High, null);
         }
 
         /// <summary>
@@ -60,9 +80,15 @@ namespace Fynydd.Halide
         /// <param name="key">Unique name of the cached item</param>
         /// <param name="value">String value to store in the cache</param>
         /// <param name="expirationSeconds">Expiration time, in seconds, from the current date and time.</param>
-        public static void Cache(string key, string value, int expirationSeconds)
+        /// <param name="context">HttpContext; defaults to Current</param>
+        public static void Cache(string key, string value, int expirationSeconds, HttpContext context = null)
         {
-            HttpContext.Current.Cache.Add(key, value, null, DateTime.Now.AddSeconds(Convert.ToDouble(expirationSeconds)), System.Web.Caching.Cache.NoSlidingExpiration, System.Web.Caching.CacheItemPriority.Normal, null);
+            if (context == null)
+            {
+                context = HttpContext.Current;
+            }
+
+            context.Cache.Add(key, value, null, DateTime.Now.AddSeconds(Convert.ToDouble(expirationSeconds)), System.Web.Caching.Cache.NoSlidingExpiration, System.Web.Caching.CacheItemPriority.Normal, null);
         }
 
         /// <summary>
@@ -71,9 +97,15 @@ namespace Fynydd.Halide
         /// <param name="key">Unique name of the cached item</param>
         /// <param name="value">Object to store in the cache</param>
         /// <param name="expirationDateTime">Expiration date and time.</param>
-        public static void Cache(string key, object value, DateTime expirationDateTime)
+        /// <param name="context">HttpContext; defaults to Current</param>
+        public static void Cache(string key, object value, DateTime expirationDateTime, HttpContext context = null)
         {
-            HttpContext.Current.Cache.Add(key, value, null, expirationDateTime, System.Web.Caching.Cache.NoSlidingExpiration, System.Web.Caching.CacheItemPriority.Normal, null);
+            if (context == null)
+            {
+                context = HttpContext.Current;
+            }
+
+            context.Cache.Add(key, value, null, expirationDateTime, System.Web.Caching.Cache.NoSlidingExpiration, System.Web.Caching.CacheItemPriority.Normal, null);
         }
 
         /// <summary>
@@ -82,43 +114,87 @@ namespace Fynydd.Halide
         /// <param name="key">Unique name of the cached item</param>
         /// <param name="value">String value to store in the cache</param>
         /// <param name="expirationDateTime">Expiration date and time.</param>
-        public static void Cache(string key, string value, DateTime expirationDateTime)
+        /// <param name="context">HttpContext; defaults to Current</param>
+        public static void Cache(string key, string value, DateTime expirationDateTime, HttpContext context = null)
         {
-            HttpContext.Current.Cache.Add(key, value, null, expirationDateTime, System.Web.Caching.Cache.NoSlidingExpiration, System.Web.Caching.CacheItemPriority.Normal, null);
+            if (context == null)
+            {
+                context = HttpContext.Current;
+            }
+
+            context.Cache.Add(key, value, null, expirationDateTime, System.Web.Caching.Cache.NoSlidingExpiration, System.Web.Caching.CacheItemPriority.Normal, null);
         }
 
         /// <summary>
         /// Output Caching wrapper method. Caches an object for a number of seconds specified
-        /// in the web.config file under the Halide Settings key "OutputCacheTime".
+        /// in the web.config file under the Halide Settings key "OutputCache.Expiration.Seconds".
         /// </summary>
         /// <param name="key">Unique name of the cached item</param>
         /// <param name="value">Object to store in the cache</param>
-        public static void Cache(string key, object value)
+        /// <param name="context">HttpContext; defaults to Current</param>
+        public static void Cache(string key, object value, HttpContext context = null)
         {
-            HttpContext.Current.Cache.Add(key, value, null, DateTime.Now.AddSeconds(Convert.ToDouble(OutputCacheTime)), System.Web.Caching.Cache.NoSlidingExpiration, System.Web.Caching.CacheItemPriority.Normal, null);
+            if (context == null)
+            {
+                context = HttpContext.Current;
+            }
+
+            context.Cache.Add(key, value, null, DateTime.Now.AddSeconds(Convert.ToDouble(OutputCacheTime)), System.Web.Caching.Cache.NoSlidingExpiration, System.Web.Caching.CacheItemPriority.Normal, null);
         }
 
         /// <summary>
         /// Output Caching wrapper method. Caches a string value for a number of seconds specified
-        /// in the web.config file under the Halide Settings key "OutputCacheTime".
+        /// in the web.config file under the Halide Settings key "OutputCache.Expiration.Seconds".
         /// </summary>
         /// <param name="key">Unique name of the cached item</param>
         /// <param name="value">String value to store in the cache</param>
-        public static void Cache(string key, string value)
+        /// <param name="context">HttpContext; defaults to Current</param>
+        public static void Cache(string key, string value, HttpContext context = null)
         {
-            HttpContext.Current.Cache.Add(key, value, null, DateTime.Now.AddSeconds(Convert.ToDouble(OutputCacheTime)), System.Web.Caching.Cache.NoSlidingExpiration, System.Web.Caching.CacheItemPriority.Normal, null);
+            if (context == null)
+            {
+                context = HttpContext.Current;
+            }
+
+            context.Cache.Add(key, value, null, DateTime.Now.AddSeconds(Convert.ToDouble(OutputCacheTime)), System.Web.Caching.Cache.NoSlidingExpiration, System.Web.Caching.CacheItemPriority.Normal, null);
         }
 
         /// <summary>
         /// Clear the output cache.
         /// </summary>
-        public static void CacheClear()
+        /// <param name="context">HttpContext; defaults to Current</param>
+        public static void CacheClear(HttpContext context = null)
         {
-            IDictionaryEnumerator CacheEnum = HttpContext.Current.Cache.GetEnumerator();
+            if (context == null)
+            {
+                context = HttpContext.Current;
+            }
+
+            IDictionaryEnumerator CacheEnum = context.Cache.GetEnumerator();
 
             while (CacheEnum.MoveNext())
             {
-                HttpContext.Current.Cache.Remove(CacheEnum.Key.ToString());
+                context.Cache.Remove(CacheEnum.Key.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Delete a single cached item.
+        /// </summary>
+        /// <param name="context">HttpContext; defaults to Current</param>
+        public static void CacheDelete(string key, HttpContext context = null)
+        {
+            if (context == null)
+            {
+                context = HttpContext.Current;
+            }
+
+            if (string.IsNullOrWhiteSpace(key) == false)
+            {
+                if (CacheValid(key, context) == true)
+                {
+                    context.Cache.Remove(key);
+                }
             }
         }
 
@@ -126,17 +202,93 @@ namespace Fynydd.Halide
         /// Output caching wrapper method. Determines if a cached item exists or not.
         /// </summary>
         /// <param name="key">Unique name of the cached item</param>
+        /// <param name="context">HttpContext; defaults to Current</param>
         /// <returns>true if cached item exists, false if not.</returns>
-        public static bool CacheValid(string key)
+        public static bool CacheValid(string key, HttpContext context = null)
         {
             bool result = false;
 
-            if (HttpContext.Current.Cache[key] != null)
+            if (context == null)
+            {
+                context = HttpContext.Current;
+            }
+
+            if (context.Cache[key] != null)
             {
                 result = true;
             }
 
             return result;
+        }
+
+        /// <summary>
+        /// Output caching wrapper method. Retrieve a typed cache item.
+        /// </summary>
+        /// <param name="key">Unique name of the cached item</param>
+        /// <param name="context">HttpContext; defaults to Current</param>
+        /// <returns>Cached value</returns>
+        public static T Cache<T>(string key, HttpContext context = null)
+        {
+            if (context == null)
+            {
+                context = HttpContext.Current;
+            }
+
+            if (string.IsNullOrWhiteSpace(key) == false)
+            {
+                if (CacheValid(key, context))
+                {
+                    var value = context.Cache[key];
+
+                    if (typeof(T) == typeof(string))
+                    {
+                        return (T)(value == null ? (object)string.Empty : Convert.ChangeType(value, typeof(T)));
+                    }
+
+                    else if (typeof(T) == typeof(bool))
+                    {
+                        return (T)(value == null ? (object)false : Convert.ChangeType(value, typeof(T)));
+                    }
+
+                    else if (typeof(T) == typeof(int) || typeof(T) == typeof(decimal) || typeof(T) == typeof(Decimal) || typeof(T) == typeof(double) || typeof(T) == typeof(Double) || typeof(T) == typeof(float) || typeof(T) == typeof(Int16) || typeof(T) == typeof(Int32) || typeof(T) == typeof(Int64) || typeof(T) == typeof(Single) || typeof(T) == typeof(short) || typeof(T) == typeof(long))
+                    {
+                        return (T)(value == null ? (object)-1 : Convert.ChangeType(value, typeof(T)));
+                    }
+
+                    else if (typeof(T) == typeof(DateTime))
+                    {
+                        return (T)(value == null ? (object)DateTime.MinValue : Convert.ChangeType(value, typeof(T)));
+                    }
+
+                    else
+                    {
+                        return (T)Convert.ChangeType(value, typeof(T));
+                    }
+                }
+
+                else
+                {
+                    if (typeof(T) == typeof(string))
+                    {
+                        return (T)Convert.ChangeType("", typeof(T));
+                    }
+
+                    else if (typeof(T) == typeof(DateTime))
+                    {
+                        return (T)Convert.ChangeType(DateTime.MinValue, typeof(T));
+                    }
+
+                    else
+                    {
+                        return (T)(object)null;
+                    }
+                }
+            }
+
+            else
+            {
+                return (T)(object)null;
+            }
         }
 
         #endregion
